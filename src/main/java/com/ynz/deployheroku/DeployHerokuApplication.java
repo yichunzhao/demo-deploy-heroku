cec5cb2c6e5a5fd85952dbc540d77a7a1da1a23e
yichunzhao <zhaoyichun@hotmail.com>
2020-07-21 03:07:01 +0200

    init commit

---
+++ src/main/java/com/ynz/deployheroku/DeployHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.deployheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployHerokuApplication.class, args);
+	}
+
+}
